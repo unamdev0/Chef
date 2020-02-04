101d771f77acf72a36c88c38a41936737fc55bb6
Udit Namdev <unamdev0@gmail.com>
2020-02-04 23:57:41 +0530

    Working on receipe page

--- client/src/components/receipes/receipe.js
+++ client/src/components/receipes/receipe.js
@@ -1,12 +1,55 @@
-import React from 'react'
-import './receipe.css'
+import React from "react";
+import "./receipe.css";
 
 const Receipe = props => {
-    const receipeData =(props.receipeData);
-    return (
-        <div className="main_wrapper">
-            <h1>{receipeData.title}</h1>
-        </div>
-    );
+  const receipeData = props.receipeData;
+  let points = () => {
+    var Arr = receipeData.instructions.split("\n");
+    Arr = Arr.filter(function(el) {
+      return el;
+    });
+    if (Arr[0] == "Instructions") {
+      Arr.splice(0, 1);
+    }
+    return Arr.map(instruction => {
+      return <li>{instruction}</li>;
+    });
   };
-  export default Receipe;
+
+  let ingredients = () => {
+    return receipeData.usedIngredients.map(ingredient => {
+      return <li>{ingredient.originalString}</li>;
+    });
+  };
+
+  let MissingIngredients = () => {
+    if (receipeData.missedIngredientCount != 0) {
+      return (
+        <div>
+          Missing Ingredients:
+          <ul>
+            {receipeData.missedIngredients.map(ingredient => {
+              return <li>{ingredient.originalString}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return;
+    }
+  };
+
+  return (
+    <div className="main_wrapper">
+      <h2>{receipeData.title}</h2>
+      <img src={receipeData.image} />
+      <div>
+        Ingredients:<ul>{ingredients()}</ul>
+      </div>
+        {MissingIngredients()}
+        <div>Instructions:
+      <ul>{points()}</ul></div>
+    </div>
+  );
+};
+export default Receipe;
